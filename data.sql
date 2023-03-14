0da5e42825897616bcce2afad50f8347cc16ff8e
mauri-jklein <mauri-jklein@educar.rs.gov.br>
2023-03-14 11:42:44 -0300

    data.sql

--- data.sql
+++ data.sql
@@ -21,7 +21,20 @@ create table if not exists pedido(
 
 create table if not exists item_pedido(
 	id serial primary key,
-	valot decimal not null,
+	valor decimal not null,
 	id_pedido int references pedido,
 	id_produto int references produto
 );
+--inserindo clientes
+insert into cliente values(default, 'bruno klein', 'brunoklein@gmail.com', '1234'); 
+insert into cliente values(default, 'mauri klein', 'maurijklein@gmail.com', '1234'); 
+--inserindo produtos
+insert into produto values(default, 'mouse', 'mouse gamer exbom', 67.90);
+insert into produto values(default, 'notebook', 'intel core i7 - 2.8 GHz', 5824.50);
+--inserindo pedido
+insert into pedido values(default, default, 5067.89, 1);
+insert into pedido values(default, default, 4999.99, 2);
+--inserindo item_pedido
+insert into item_pedido values(default, 67.90, 1, 1);
+insert into item_pedido values(default, 4999.99, 1, 2);
+insert into item_pedido values(default, 4999.99, 2, 2);
